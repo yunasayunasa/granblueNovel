86ac2accc360d93f7962592b5615cc8a48a5aeb3
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-05-26 16:29:04 +0900

    haikei

--- js/storage.js
+++ js/storage.js
@@ -6,3 +6,12 @@ monogatari.storage ({
 		name: ''
 	}
 });
+
+// 背景画像の定義 (例)
+monogatari.assets('scenes', {
+    'bg_calc_space': 'dummy_background.jpg', // プロローグの演算空間用 (仮)
+    'bg_cafe': 'dummy_background.jpg',           // カフェ用 (仮)
+    'bg_deck': 'dummy_background.jpg',           // 甲板用 (仮)
+    // 他のシーン用の背景もここに追加していく
+    'title_background': 'dummy_background.jpg' // タイトル画面用 (仮)
+});
