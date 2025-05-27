3b4e95ba90a4bd88f8ee0d827e0063ea345b19f5
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-05-27 16:12:19 +0900

    asupect

--- js/options.js
+++ js/options.js
@@ -162,7 +162,7 @@ monogatari.preferences ({
 
 	// Initial resolution used for Electron, it must match the settings inside
 	// the electron.js file. This has no effect on web deployed novels.
-	'Resolution': '450x800',
+	'Resolution': '800x450',
 
 	// Speed at which dialog text will appear
 	'TextSpeed': 20,
