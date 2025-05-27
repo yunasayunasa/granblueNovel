a513da043b7da114b6a25372712c152a2bcc52a4
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-05-27 16:27:38 +0900

    ｓｓ

--- js/options.js
+++ js/options.js
@@ -148,7 +148,7 @@ monogatari.settings({
 monogatari.preferences ({
 
 	// Initial Language for Multilanguage Games or for the Default GUI Language.
-	'Language': '日本語',
+	'Language': 'English',
 
 	
 
